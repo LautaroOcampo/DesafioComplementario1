65f4fb34fe5e4e004eed8d58d2a0c283489d91c8
tato <tato.ocampo@live.com.ar>
2022-06-01 01:34:16 -0300

    Desafio Complementario 1

---
+++ app.js
@@ -0,0 +1,11 @@
+
+let nombre = prompt("Cual es tu nombre")
+
+let repeticiones = prompt("Cuantas veces queres que lo repitamos?")
+
+for(let i = 0 ; i < repeticiones ; i++){
+    console.log(nombre)
+}
+
+
+
